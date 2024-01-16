27949356909c8ca6f68e106680f4bb819db6f0f5
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-01-16 14:19:34 +0100

    remis dans l'odre et nom variable exsae105.py

--- exsae105.py
+++ exsae105.py
@@ -2,21 +2,23 @@
 #                       sortie des infos
 ####################################################################################
 
-def composant1():
+def affichage():
     f = open("composants.csv", "r") #on ouvre le fichier en 1 et on "r" lis le fichier
-    refcomp=[] #on creer une liste pour y stoquer les part 1 du csv trouver
+    liste=[] #on creer une liste pour y stoquer les part 1 du csv trouver
 
     
     for elt in f: #pour tout element dans f
-        a=elt.split("\n") #a est = aux elements separer par un espace
+        elmgauche=elt.split(";")
+        liste.append(elmgauche[0]) #on ajoute a la liste trouver précédement les element en premier que l'on a separer
+        
+        elmligne=elt.split("\n") #a est = aux elements separer par un espace
 
-        b=elt.split(";")
-        refcomp.append(b[0]) #on ajoute a la liste trouver précédement les element en premier que l'on a separer
-        print(b[0],"=", a)
+        
+        print(elmgauche[0],"=", elmligne)
 
- 
+
 ###############################################################################
 #                     apl fonction
 ##############################################################################
 
-composant1()
+affichage()
